8fa07b444dffd5239fde4827701c4768ef665f42
sbalexandru <sbalexandru@yahoo.com>
2022-03-19 23:58:17 +0200

    start create image box for pages

--- src/page/WheelPage.js
+++ src/page/WheelPage.js
@@ -1,15 +1,28 @@
 import React from "react";
-import { Container } from "./WheelPage.styled";
+import {
+  Container,
+  Box,
+  Title,
+  Text,
+  Image,
+  ImageBox,
+} from "./WheelPage.styled";
 import { WheelImages } from "../components/WheelImages";
+// import i18n from "../i18n";
 
 const WheelPage = () => {
   return (
     <Container>
-      <h1>wheel</h1>
-      <img src={WheelImages.Audi1} alt="audi" />
-      <img src={WheelImages.Audi2} alt="audi" />
-      <img src={WheelImages.Audi3} alt="audi" />
-      <img src={WheelImages.Audi4} alt="audi" />
+      <Box>
+        <Title>Title</Title>
+        <Text>audi valami</Text>
+        <ImageBox>
+          <Image src={WheelImages.Audi1} alt="audi" />
+          <Image src={WheelImages.Audi2} alt="audi" />
+          <Image src={WheelImages.Audi3} alt="audi" />
+          <Image src={WheelImages.Audi4} alt="audi" />
+        </ImageBox>
+      </Box>
     </Container>
   );
 };
--- src/page/WheelPage.styled.js
+++ src/page/WheelPage.styled.js
@@ -5,3 +5,25 @@ export const Container = styled.div`
   /* height: 1500px; */
   background: yellow;
 `;
+
+export const Box = styled.div`
+  width: 80%;
+  margin: auto;
+`;
+
+export const Title = styled.h2`
+  font-size: x-large;
+`;
+
+export const Text = styled.p`
+  font-size: 16px;
+`;
+
+export const ImageBox = styled.div`
+  width: 100%;
+`;
+
+export const Image = styled.img`
+  width: 90%;
+  margin: auto;
+`;
